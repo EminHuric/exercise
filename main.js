b266b948502ca99228d89881084ceae25a0a7ae0
emynhrx <emynbusiness@gmail.com>
2025-04-16 14:40:53 +0200

    newww

--- main.js
+++ main.js
@@ -2,20 +2,35 @@ const button = document.getElementById('button');
 const content = document.getElementById('content');
 
 function add() {
-    const paragraph = document.createElement('p');
-    paragraph.textContent = prompt('Enter some text:');
-    paragraph.classList.add("frame");
-    content.appendChild(paragraph);
-//// remove notes
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.classList.add("remove-button");
+const paragraph = document.createElement('p');
+paragraph.textContent = prompt("enter some text");
+content.appendChild(paragraph);
+paragraph.classList.add("frame");
 
-    removeButton.addEventListener('click', function() {
-        content.removeChild(paragraph);
-    });
-    
+/// remove button
+
+const removeButton = document.createElement("button");
+removeButton.textContent = "Remove";
+removeButton.classList.add("remove-button");
+paragraph.appendChild(removeButton);
+removeButton.addEventListener("click", function() {
+
+content.removeChild(paragraph);
+})
+
+//edit
+
+const editButton = document.createElement('button');
+editButton.textContent = 'Edit';
+paragraph.appendChild(editButton);
+editButton.classList.add("edit-button");
+
+editButton.addEventListener('click', function() {
+    const newText = prompt("enter new text");
+    paragraph.textContent = newText
     paragraph.appendChild(removeButton);
+    paragraph.appendChild(editButton);
+});
 
 }
 
