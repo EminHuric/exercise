7bc8cc8b496fc77538fb592da0d52b1ab2b05fc6
bodln <omer.sadikovic2001@gmail.com>
2025-04-26 17:43:10 +0200

    Primena formatiranje radi lakseg citanja.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const button = document.getElementById('button');
-const content = document.getElementById('content');
-const bars = document.getElementById('bars');
-const menu = document.getElementById('menuBar');
-const close = document.getElementById('close');
+const button = document.getElementById("button");
+const content = document.getElementById("content");
+const bars = document.getElementById("bars");
+const menu = document.getElementById("menuBar");
+const close = document.getElementById("close");
 
-bars.addEventListener('click', function () {
-    menu.style.display = 'block'; // Ovdje prikazujemo meni
+bars.addEventListener("click", function () {
+  menu.style.display = "block"; // Ovdje prikazujemo meni
 });
-close.addEventListener('click', function () {
-    menu.style.display = 'none'; 
+close.addEventListener("click", function () {
+  menu.style.display = "none";
 });
 
 let notes = [];
@@ -17,117 +17,116 @@ let new_id = 0;
 
 function add() {
   /// window
-  const window = document.createElement('div');
-  window.classList.add('window');
+  const window = document.createElement("div");
+  window.classList.add("window");
   document.body.appendChild(window);
 
-  const enter = document.createElement('div');
-  enter.classList.add('enter');
+  const enter = document.createElement("div");
+  enter.classList.add("enter");
   window.appendChild(enter);
 
   /// add input
-  const enterTitle = document.createElement('input');
-  enterTitle.type = 'text';
-  enterTitle.placeholder = 'Enter a title';
-  enter.appendChild(enterTitle)
-  enterTitle.classList.add('enterTitle')
+  const enterTitle = document.createElement("input");
+  enterTitle.type = "text";
+  enterTitle.placeholder = "Enter a title";
+  enter.appendChild(enterTitle);
+  enterTitle.classList.add("enterTitle");
 
-  const enterNote = document.createElement('input');
-  enterNote.type = 'text';
-  enterNote.placeholder = 'Enter a note';
+  const enterNote = document.createElement("input");
+  enterNote.type = "text";
+  enterNote.placeholder = "Enter a note";
   enter.appendChild(enterNote);
-  enterNote.classList.add('enterNote');
+  enterNote.classList.add("enterNote");
 
   /// add button
-  const addButton = document.createElement('button');
-  addButton.textContent = 'Add';
-  addButton.classList.add('addButton');
+  const addButton = document.createElement("button");
+  addButton.textContent = "Add";
+  addButton.classList.add("addButton");
   enter.appendChild(addButton);
 
   /// close button
-  const closeButton = document.createElement('button');
-  closeButton.textContent = 'Close';
-  closeButton.classList.add('closeButton');
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "Close";
+  closeButton.classList.add("closeButton");
   enter.appendChild(closeButton);
-  closeButton.addEventListener('click', function () {
+  closeButton.addEventListener("click", function () {
     document.body.removeChild(window);
   });
 
-  addButton.addEventListener('click', function () {
+  addButton.addEventListener("click", function () {
     const titleValue = enterTitle.value;
     const noteValue = enterNote.value;
 
-notes.push({
+    notes.push({
       id: ++new_id,
       title: titleValue,
       note: noteValue,
     });
 
-    const note = document.createElement('div');
-    note.classList.add('note');
+    const note = document.createElement("div");
+    note.classList.add("note");
     note.innerHTML = `<h3>${titleValue}</h3><p>${noteValue}</p>`;
     content.appendChild(note);
     document.body.removeChild(window);
 
     /// remove button
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = '<i class="fas fa-trash"></i>'; 
-    removeButton.classList.add('remove-button')
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+    removeButton.classList.add("remove-button");
     note.appendChild(removeButton);
-    removeButton.addEventListener('click', function() {
+
+    removeButton.addEventListener("click", function () {
       note.remove();
     });
 
     // edit button
-    const editButton = document.createElement('button');
+    const editButton = document.createElement("button");
     editButton.innerHTML = '<i class="fas fa-pen"></i>';
     note.appendChild(editButton);
-    editButton.classList.add('edit-button");
+    editButton.classList.add("edit-button");
     
     editButton.dataset.id = new_id; // <------------------------------------------------------
 
     editButton.addEventListener("click", function () {
-let thisNote = notes.find((n) => n.id == editButton.dataset.id);
+      let thisNote = notes.find((n) => n.id == editButton.dataset.id);
       if (note) {
         console.log(thisNote.title);
         console.log(thisNote.note);
       }
     });
-  
-  const p = note.querySelector('p');
-///bold
-  const boldButton = document.createElement('button');
-  boldButton.innerHTML = '<i class="fa-sharp fa-solid fa-bold"></i>';
-  note.appendChild(boldButton);
-  boldButton.classList.add('b-button');
 
-boldButton.addEventListener('click', function() {
-  p.classList.toggle('bold-button');
-});
+    const p = note.querySelector("p");
+    ///bold
+    const boldButton = document.createElement("button");
+    boldButton.innerHTML = '<i class="fa-sharp fa-solid fa-bold"></i>';
+    note.appendChild(boldButton);
+    boldButton.classList.add("b-button");
 
-///italic
-const italicButton = document.createElement('button');
-italicButton.innerHTML = '<i class="fa-sharp fa-solid fa-italic"></i>';
-note.appendChild(italicButton);
-italicButton.classList.add('i-button');
+    boldButton.addEventListener("click", function () {
+      p.classList.toggle("bold-button");
+    });
 
-italicButton.addEventListener('click', function() {
-    p.classList.toggle('italic-button');
-})
+    ///italic
+    const italicButton = document.createElement("button");
+    italicButton.innerHTML = '<i class="fa-sharp fa-solid fa-italic"></i>';
+    note.appendChild(italicButton);
+    italicButton.classList.add("i-button");
 
-/// justify
+    italicButton.addEventListener("click", function () {
+      p.classList.toggle("italic-button");
+    });
 
-const justifyButton = document.createElement('button');
-justifyButton.innerHTML = '<i class="fa-solid fa-align-justify"></i>';
-note.appendChild(justifyButton);
-justifyButton.classList.add('j-button');
+    /// justify
 
-justifyButton.addEventListener('click', function() {
-    p.classList.toggle('justify-button');
-})
-  
-});
+    const justifyButton = document.createElement("button");
+    justifyButton.innerHTML = '<i class="fa-solid fa-align-justify"></i>';
+    note.appendChild(justifyButton);
+    justifyButton.classList.add("j-button");
 
+    justifyButton.addEventListener("click", function () {
+      p.classList.toggle("justify-button");
+    });
+  });
 }
 
-button.addEventListener('click', add);
+button.addEventListener("click", add);
